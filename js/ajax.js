03b9581c4bd4a05f4e4432f44cc75cd04fa100aa
siddharthHestabit <115151585+siddharthHestabit@users.noreply.github.com>
2022-10-14 17:24:23 +0530

    Updated launchpad

--- js/ajax.js
+++ js/ajax.js
@@ -4,13 +4,13 @@ function get_data(maxPage, testimonialId_name) {
     if (currentPage > maxPage) {
         jQuery('#loadId').hide();
     } else {
-        // Do currentPage + 1, because we want to load the next page
         jQuery.ajax({
             type: 'POST',
-            url: '/wp-launchpad/wp-admin/admin-ajax.php',
+            url: ajax_var.url,
             dataType: 'html',
             data: {
                 action: 'weichie_load_more_btn',
+                nonce: ajax_var.nonce,
                 paged: currentPage,
                 testimonialId_name: testimonialId_name,
             },
@@ -25,7 +25,6 @@ let maxquestion = 1;
 function add_more_field(questionId, questionName, answerId, answerName) {
     questionName = questionName.slice(0, -1);
     answerName = answerName.slice(0, -1);
-    // alert(questionId + " \n" + questionName + " \n" + answerId + " \n" + answerName);
     i++;
     questionId = questionId + '-' + i;
     questionName = questionName + i + ']';
@@ -35,10 +34,11 @@ function add_more_field(questionId, questionName, answerId, answerName) {
     if (maxquestion <= 5) {
         jQuery.ajax({
             type: 'POST',
-            url: '/wp-launchpad/wp-admin/admin-ajax.php',
+            url: ajax_var.url,
             dataType: 'html',
             data: {
                 action: 'add_more',
+                nonce: ajax_var.nonce,
                 questionId: questionId,
                 questionName: questionName,
                 answerId: answerId,
@@ -53,6 +53,44 @@ function add_more_field(questionId, questionName, answerId, answerName) {
     }
 }
 
+jQuery(document).ready(function () {
+    jQuery('.register').on('click', function () {
+        var fst_name = jQuery('#fst_name').val();
+        var lst_name = jQuery('#lst_name').val();
+        var mobile_number = jQuery('#mobile_number').val();
+        var email = jQuery('#email').val();
+        var password = jQuery('#password').val();
+        if (fst_name.length == 0) {
+            jQuery('.fst_name_error').html("<p>* First Name can not Empty</p>");
+        } else if (lst_name.length == 0) {
+            jQuery('.lst_name_error').html("<p>* Last Name can not Empty</p>");
+        } else if (mobile_number.length == 0) {
+            jQuery('.mobile_error').html("<p>* Mobile number can not Empty</p>");
+        }
+        else if (email.length == 0) {
+            jQuery('.email_error').html("<p>* Email can not Empty</p>");
+        } else if (password.length == 0) {
+            jQuery('.password_error').html("<p>* password can not Empty</p>");
+        } else {
+            jQuery.ajax({
+                type: 'POST',
+                url: ajax_var.url,
+                dataType: 'html',
+                data: {
+                    action: 'register_form',
+                    nonce: ajax_var.nonce,
+                    name: fst_name,
+                    email: email,
+                    password: password
+                },
+                success: function (res) {
+                    alert(res);
+                }
+            });
+        }
+
+    });
+});
 
 
 
